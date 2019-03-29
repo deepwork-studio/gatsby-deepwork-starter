8eabdfa077e2587e7663dc9e8bf7e082bdc613ae
Jeff Reiner <theundeadghost@gmail.com>
2019-03-29 11:57:22 +0100

    :art: running prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/SEO";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    
+
     <Heading as="h1">Hi peeps</Heading>
     <Text as="p">Welcome to your new Deep Work Studio site.</Text>
     <Text as="p">Let's get building!</Text>
